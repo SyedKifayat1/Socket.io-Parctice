88502f7b6d3b7969ae3d766a4fd00b2e68325e04
Syed Kifayat <bsce22026@itu.edu.pk>
2025-02-03 01:07:03 +0500

    commit

--- socket-io-project-4/slackClone/public/joinNs.js
+++ socket-io-project-4/slackClone/public/joinNs.js
@@ -47,3 +47,17 @@
 
 
 
+const joinNs = (element,nsData) => {
+    const nsEndpoint = element.getAttribute("ns");
+    console.log(nsEndpoint);
+    const clickNs = nsData.find(row => row.endpoint === nsEndpoint);
+    const rooms = clickNs.rooms;
+    const roomList = document.querySelector(".room-list");
+    roomList.innerHTML = "";
+    console.log(roomList);
+    rooms.forEach(room => {
+        roomList.innerHTML += `<li><i class="fa-solid fa-lock"></i> ${room.roomTitle}</li>`
+    })
+
+    localStorage.setItem("lastNs", nsEndpoint);
+}
--- socket-io-project-4/slackClone/public/scripts.js
+++ socket-io-project-4/slackClone/public/scripts.js
@@ -1,5 +1,8 @@
-const userName = prompt("What is your username?");
-const password = prompt("What is your Password?");  
+// const userName = prompt("What is your username?");
+// const password = prompt("What is your Password?");  
+
+const userName = "Kifayat";
+const password = "x";
 
 const socket = io("http://localhost:9000");
 
@@ -11,9 +14,19 @@ socket.on("connect",()=>{
 
 // listen for the nsList event from the server which give us the namespace
 socket.on("nsList",(nsData)=>{
+    const lastNs = localStorage.getItem("lastNs");
     console.log("Received message from server: ", nsData);
     const namespacesDiv = document.querySelector(".namespaces");
+    namespacesDiv.innerHTML = "";
     nsData.forEach(ns=>{
-        namespacesDiv.innerHTML += ` <div class="namespace" ns="${ns.name}"><img src="${ns.image}"></div>`
+        namespacesDiv.innerHTML += ` <div class="namespace" ns="${ns.endpoint}"><img src="${ns.image}"></div>`
     })
+    Array.from(document.getElementsByClassName("namespace")).forEach(element=>{
+        console.log(element);
+        element.addEventListener("click",()=>{
+            joinNs(element,nsData);
+        });
+
+    })
+    joinNs(document.getElementsByClassName("namespace")[0],nsData);
 })
