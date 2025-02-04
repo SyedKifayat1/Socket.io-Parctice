e56824b4fd89b7d655b1b1a586d2d23ce09126aa
Syed Kifayat <bsce22026@itu.edu.pk>
2025-02-04 14:05:17 +0500

    commit

--- socket-io-project-4/namespaces/namespaces.js
+++ socket-io-project-4/namespaces/namespaces.js
@@ -11,12 +11,21 @@ const io = socket(expressServer);
 
 
 io.on('connection', (socket) => {
+    io.of("/admin").emit("userJoinedMainNS","MainNS");
+    // io.of("/").on('connection', (socket) => {
     console.log("Client connected: " + socket.id);
     // in ws we use "sesd" method to connect and in socket.io we use "emit" method to connect
     socket.emit("messageFromServer", { data: "welcome to the socket server" })
+    // socket.emit("messageFromServer", { data: "welcome to the socket server" })
     socket.on("newMessageToServer", (dataFromClient) => {
         console.log("Received message: ", dataFromClient.text);
         io.emit("newMessageToClient", { text: dataFromClient.text })
 
     })
 })
+
+
+io.of("/admin").on('connection', (socket) => {
+    console.log("Client connected through admin: " + socket.id);
+    io.of("/admin").emit("MessageFromAdmin",{data:"hi this is message from admin server side"})
+});
